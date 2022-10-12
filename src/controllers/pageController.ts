cdcd7ee23119b2790a57f4d8e0ecf43dc61477f3
Diogo Schoninger <58345713+diogoschoninger@users.noreply.github.com>
2022-10-12 18:27:11 -0300

    Activating the navbar items

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,25 +1,30 @@
 import { Request, Response } from "express"
+import { createMenuObject } from '../helpers/createMenuObject'
 
 export const home = (req: Request, res: Response) => {
   res.render('pages/page', {
+    menu: createMenuObject('all'),
     title: 'All animals'
   })
 }
 
 export const dogs = (req: Request, res: Response) => {
   res.render('pages/page', {
+    menu: createMenuObject('dog'),
     title: 'Dogs'
   })
 }
 
 export const cats = (req: Request, res: Response) => {
   res.render('pages/page', {
+    menu: createMenuObject('cat'),
     title: 'Cats'
   })
 }
 
 export const fishes = (req: Request, res: Response) => {
   res.render('pages/page', {
+    menu: createMenuObject('fish'),
     title: 'Fishes'
   })
 }
