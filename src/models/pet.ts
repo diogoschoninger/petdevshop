af832cfc1601841dbd0978464918e5052a4d45c3
Diogo Schoninger <schoninger.diogo@gmail.com>
2022-10-14 20:56:00 -0300

    Listing items in controller

--- src/models/pet.ts
+++ src/models/pet.ts
@@ -5,7 +5,7 @@ interface Pet {
   image: string,
   name: string,
   color: string,
-  sex: 'Male' | 'Female'
+  genre: 'Male' | 'Female'
 }
 
 const data: Pet[] = [
@@ -14,119 +14,119 @@ const data: Pet[] = [
     image: 'pastor-alemao.jpg',
     name: 'Pastor-alemão',
     color: 'Yellow and black',
-    sex: 'Male'
+    genre: 'Male'
   },
   {
     type: 'dog',
     image: 'labrador.jpg',
     name: 'Labrador-retriever',
     color: 'White',
-    sex: 'Male'
+    genre: 'Male'
   },
   {
     type: 'dog',
     image: 'zwergspitz.jpg',
     name: 'Zwergspitz',
     color: 'Yellow',
-    sex: 'Female'
+    genre: 'Female'
   },
   {
     type: 'dog',
     image: 'husky.jpg',
     name: 'Husky Siberiano',
     color: 'Black and white',
-    sex: 'Male'
+    genre: 'Male'
   },
   {
     type: 'dog',
     image: 'golden.jpg',
     name: 'Golden Retriever',
     color: 'Yellow',
-    sex: 'Male'
+    genre: 'Male'
   },
   {
     type: 'dog',
     image: 'poodle.jpg',
     name: 'Poodle',
     color: 'White',
-    sex: 'Female'
+    genre: 'Female'
   },
   {
     type: 'dog',
     image: 'bulldog.jpg',
     name: 'Bulldog',
     color: 'White and yellow',
-    sex: 'Male'
+    genre: 'Male'
   },
   {
     type: 'cat',
     image: 'persa.jpg',
     name: 'Persa',
     color: 'Yellow',
-    sex: 'Male'
+    genre: 'Male'
   },
   {
     type: 'cat',
     image: 'mainecoon.jpg',
     name: 'Maine Coon',
     color: 'Black and white',
-    sex: 'Male'
+    genre: 'Male'
   },
   {
     type: 'cat',
     image: 'bengal.jpg',
     name: 'Bengal',
     color: 'Black, white and yellow',
-    sex: 'Female'
+    genre: 'Female'
   },
   {
     type: 'cat',
     image: 'siames.jpg',
     name: 'Siamês',
     color: 'Yellow and black',
-    sex: 'Male'
+    genre: 'Male'
   },
   {
     type: 'cat',
     image: 'sphynx.jpg',
     name: 'Sphynx',
     color: 'White',
-    sex: 'Male'
+    genre: 'Male'
   },
   {
     type: 'fish',
     image: 'neon.jpg',
     name: 'Tetra Neon',
     color: 'Red and blue',
-    sex: 'Male'
+    genre: 'Male'
   },
   {
     type: 'fish',
     image: 'matogrosso.jpg',
     name: 'Mato Grosso',
     color: 'Orange',
-    sex: 'Male'
+    genre: 'Male'
   },
   {
     type: 'fish',
     image: 'limpavidro.jpg',
     name: 'Limpa Vidro',
     color: 'Green and white',
-    sex: 'Male'
+    genre: 'Male'
   },
   {
     type: 'fish',
     image: 'tanictis.jpg',
     name: 'Tanictis',
     color: 'Red',
-    sex: 'Male'
+    genre: 'Male'
   },
   {
     type: 'fish',
     image: 'acara.jpg',
     name: 'Acará Bandeira',
     color: 'Black',
-    sex: 'Male'
+    genre: 'Male'
   },
 ]
 
